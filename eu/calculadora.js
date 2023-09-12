cc434e5a5cf5f73107d689fb7eb1ae0f0211d1d5
miguel arthur coelho <miguelarthurmeumine@gmail.com>
2023-09-12 15:29:11 -0300

    .

--- eu/calculadora.js
+++ eu/calculadora.js
@@ -1,9 +1,14 @@
-const notas = [1, 2, 4, 5]
-let media = 0
-for(let x = 0; x < notas.length; x++){
-    let pos = notas[x]
-    media += pos
+let nomes = ['miguel', 'mariana', 'alcineia']
+let notas = [8, 10, 4]
 
+for(let x = 0; x <nomes.length; x++) {
+    let nome = nomes[x]
+    let nota = notas[x]
+    if(nota < 6) {
+        notas.splice(x, 1)
+        nomes.splice(x, 1)
+    }
 }
-let total = media/notas.length
-console.log(total)
+
+console.log(nomes)
+console.log(notas)
---
+++ eu/x.js
@@ -0,0 +1,4 @@
+notas = [1, 4, 5, 6]
+
+notas.splice(0, 1, 6)
+console.log(notas)

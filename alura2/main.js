a61ce9851e7b0386a64ffc8621f1ec889d4f7de4
miguel arthur coelho <miguelarthurmeumine@gmail.com>
2023-11-14 16:35:20 -0300

    .

--- alura2/main.js
+++ alura2/main.js
@@ -1,18 +1,9 @@
-var main = document.querySelector('.teclado')
-var teclas = document.querySelectorAll('.tecla')
+function carregou() {
+    var conteudo = document.querySelector('#texto')
+    var data = new Date()
+    var hora = data.getHours()
+    conteudo.textContent = `agora sao ${hora} horas`
+    console.log(hora)
 
-    for(var i = 0; i < teclas.length; i++){
-        let tecla = teclas[i]
-        tecla.addEventListener('click', function(){
-        
-        var ida = tecla.classList[1]
-        qualSom(`#som_${ida}`)
-        console.log(`som_${ida}`)
-        })
-   
-}
-
-function qualSom(id){
-    var som = document.querySelector(id).play()
-    
+    if hora > 0 
 }

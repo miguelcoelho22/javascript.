57a83742937e22fc780170f04218f5c4b9ca194a
miguel arthur coelho <miguelarthurmeumine@gmail.com>
2023-12-19 15:03:54 -0300

    .

--- projetovoltar/projeto.js
+++ projetovoltar/projeto.js
@@ -1,32 +1,15 @@
 function verificar() {
-    var data = new Date();
-    var ano = data.getFullYear();
-    
-    var img = document.createElement('img');
-    img.setAttribute('id', 'foto');
 
-    var idadepessoa = document.querySelector('#txtano');
-    var idade = ano - idadepessoa.value;
-    var resultado = document.querySelector('#resultado');
-    var sex = document.getElementsByName('radsex');
-    
-    if(idadepessoa.value.length == 0 || idadepessoa.value > ano) {
-        window.alert('Você não digitou o ano de nascimento correto');
-    } else {
-        var genero = ' ';
-        if(sex[0].checked) {
-            genero = 'homem';
-            if(idade > 0 && idade < 20) {
-                img.setAttribute('src', 'foto.png');
-                resultado.appendChild(img); // Adiciona a imagem ao documento
-            } else {
-                resultado.removeChild(img); // Remove a imagem do documento se a idade não estiver no intervalo
-            }
-        } else if(sex[1].checked) {
-            genero = 'mulher';
-        } else {
-            window.alert('Sexo não selecionado');
-        }
+    var numero = parseInt(document.querySelector('#numero').value)
+    var tabu = document.querySelector('#tabuada')
+    for(let i = 0; i <= 10 ; i++) {
+        
+        let item = document.createElement('td')
+        let linha = document.createElement('tr')
+        let resultado = numero * i
+        item.textContent = `${numero} x ${i} = ${resultado}`
+        linha.appendChild(item)
+        tabu.appendChild(linha)
+
     }
-    resultado.textContent = `Detectamos um(a) ${genero} com ${idade} anos`;
-}
+}   

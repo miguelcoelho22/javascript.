25f7ac4e679bc69a5ff0d20c73a837e2ea5aadc4
miguel arthur coelho <miguelarthurmeumine@gmail.com>
2023-12-21 14:20:52 -0300

    .

--- projetovoltar/projeto.js
+++ projetovoltar/projeto.js
@@ -1,15 +1,50 @@
 function verificar() {
+    var numero = parseInt(document.querySelector('#num').value)
+    
+    if(isNaN(numero)) {
+        window.alert(' voce digitou um valor invalido')
+    }else {
+        var tabuada = document.querySelector('#resultado')
+    
+        let linha = document.createElement('td')
+        let coluna = document.createElement('tr')
+        linha.textContent = `valor ${numero} adicionado`
+        coluna.appendChild(linha)
+        tabuada.appendChild(coluna)
+    
 
-    var numero = parseInt(document.querySelector('#numero').value)
-    var tabu = document.querySelector('#tabuada')
-    for(let i = 0; i <= 10 ; i++) {
-        
-        let item = document.createElement('td')
-        let linha = document.createElement('tr')
-        let resultado = numero * i
-        item.textContent = `${numero} x ${i} = ${resultado}`
-        linha.appendChild(item)
-        tabu.appendChild(linha)
 
+    numeros.push(numero)
+
+    console.log(numeros)
     }
+    
 }   
+        
+var numeros = []
+
+function calcular() {
+    var resultado = document.querySelector('#resultado2')
+    let soma = 0
+    let menorvalor = numeros[0]
+    for(let i = 0; i < numeros.length; i++) {
+        soma += numeros[i]
+        if(numeros[i] <= menorvalor) {
+            menorvalor = numeros[i]
+        }
+    }
+    let media = soma/numeros.length
+    console.log(menorvalor)
+    
+    resultado.innerHTML = ' '
+    resultado.innerHTML += `ao todo temos ${numeros.length} valores adicionados`
+
+    
+    resultado.innerHTML += ` o menor valor adicionado é ${menorvalor}`
+
+    
+    resultado.innerHTML += ` a media dos valores adicionados é ${media} `
+   
+   
+    
+}
